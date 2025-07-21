94975c08e3dd52c766c8f03cde95c490523e7de5
Tamere147 <112899811+Tamere147@users.noreply.github.com>
2025-07-21 22:03:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
 
+  const page = await browser.newPage();
   await page.goto('https://widget.nowplaying.site/hEcrFVjEMol3fzEC', {
     waitUntil: 'domcontentloaded',
     timeout: 0,
   });
 
   try {
-    // Attente des sélecteurs complets
     await page.waitForSelector('html body div.wrapper div#app-cover.raise.active div#player div#player-content img#album.active', { timeout: 30000 });
     await page.waitForSelector('html body div.wrapper div#app-cover.raise.active div#player div#player-content div#player-track div.artists-height-fix h4#artists.active', { timeout: 30000 });
     await page.waitForSelector('html body div.wrapper div#app-cover.raise.active div#player div#player-content div#player-track h2#name.active', { timeout: 30000 });
 
-    // Scraping des données
     const artist = await page.$eval(
       'html body div.wrapper div#app-cover.raise.active div#player div#player-content div#player-track div.artists-height-fix h4#artists.active',
       el => el.textContent.trim()
