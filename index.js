98b67386aea668e4e8ba3b8a2de136a96925c670
Tamere147 <112899811+Tamere147@users.noreply.github.com>
2025-07-22 00:34:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36');
 app.get('/nowplaying', async (req, res) => {
   let browser;
   try {
@@ -12,7 +12,7 @@ app.get('/nowplaying', async (req, res) => {
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
-
+  
     const page = await browser.newPage();
     await page.goto('https://widget.nowplaying.site/hEcrFVjEMol3fzEC', {
       waitUntil: 'domcontentloaded',
