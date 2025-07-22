856a81a07eb8f9d1e49f719921137acfb2f0ec01
Tamere147 <112899811+Tamere147@users.noreply.github.com>
2025-07-22 11:22:47 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,6 +2,8 @@ import { chromium } from '@playwright/test';
 
 const browser = await chromium.launch({ headless: true });
 const page = await browser.newPage();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 await page.goto('https://widget.nowplaying.site/hEcrFVjEMol3fzEC', {
   waitUntil: 'domcontentloaded'
